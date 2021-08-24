fb43b1b4b193694fae3f4f2d074ef912b0afc7c8
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-08-24 12:10:23 +0700

    filter integrate

--- public/js/FilterComp.js
+++ public/js/FilterComp.js
@@ -4,10 +4,45 @@ Vue.component('filter-el', {
           userSearch: ''
       }
     },
-    template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
-                <input type="text" class="search-field" v-model="userSearch">
-                <button type="submit" class="btn-search">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>`
+    template: `<form action="#" class="searchBlock search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
+                        <details>
+                            <summary class="button searchSelect">Browse &#9660;</summary>
+                            <div class="subMenuArrow"></div>
+                            <nav class="mainSubMenusBlock">
+                                <div class="subMenuBox">
+                                    <div class="subMenuBoxTextInfo">
+                                        <div class="subMenuBoxTitle">COMPANY</div>
+                                        <ul class="subMenu">
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">Shop</a></li>
+                                            <li><a href="#">About</a></li>
+                                            <li><a href="#">How It Works</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">Shop</a></li>
+                                            <li><a href="#">About</a></li>
+                                            <li><a href="#">How It Works</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="subMenuBox">
+                                    <div class="subMenuBoxTextInfo">
+                                        <div class="subMenuBoxTitle">INFORMATION</div>
+                                        <ul class="subMenu">
+                                            <li><a href="#">Tearms & Condition</a></li>
+                                            <li><a href="#">Privacy Policy</a></li>
+                                            <li><a href="#">How to Buy</a></li>
+                                            <li><a href="#">How to Sell</a></li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </nav>
+                        </details>
+                        <input type="text" class="searchInputTest search-field" placeholder="Search for item..." v-model="userSearch">
+                        <button class="searchSubmit" type="submit">
+                            <img src="img/loupeIcon.png" alt="loupe">
+                        </button>
+                    </form>`
 })
